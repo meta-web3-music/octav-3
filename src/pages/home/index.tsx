dc457bbf7b3d4995b41d86991c9356fa26307287
Om More <51229945+thisisommore@users.noreply.github.com>
2023-10-18 15:36:28 +0530

    Introduce Home page (#3)

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,43 @@
+import { IonContent, IonHeader, IonPage } from "@ionic/react";
+
+const artists = [0, 0, 2, 3, 0];
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader mode="ios">
+        <div className="flex p-4 items-center relative font-figtree">
+          <p className="absolute left-[50%] translate-x-[-50%] text-xl">Home</p>
+          <img
+            src="/images/profile_octav3.png"
+            className="w-6 rounded-full ml-auto"
+          />
+        </div>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div className="p-4 w-full">
+          <h2 className="font-semibold text-xl">My Hubs</h2>
+          <div className="my-5"></div>
+          <div className="grid grid-cols-3 gap-6">
+            {artists.map((e, i) => (
+              <div key={i} className="relative">
+                <div className="w-24 mx-auto h-24 bg-[#D9D9D9] rounded-full overflow-hidden">
+                  <img
+                    src="/images/drake.jpeg"
+                    className="h-full w-full object-cover"
+                  />
+                </div>
+                <div className="my-2"></div>
+                <p className="text-center">Drake</p>
+                <div className="w-6 h-6 bg-[#F3EA01] absolute right-1 top-1 border-white border-2 rounded-full flex justify-center items-center">
+                  <p className="text-xs font-bold">4</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -3,26 +3,34 @@ import Tab1 from "./Tab1";
 import Tab2 from "./Tab2";
 import Tab3 from "./Tab3";
 import SignIn from "./signin";
+import Start from "./start";
+import React from "react";
+import Home from "./home";
 
-const routes: [string, JSX.Element, boolean?][] = [
-  ["/tab1", <Tab1 />],
-  ["/tab2", <Tab2 />],
-  ["/tab3", <Tab3 />],
-  ["/signin", <SignIn />, true],
+const routes: [string, React.FC<any> | string, boolean?, boolean?][] = [
+  // ["/tab1", Tab1, true],
+  // ["/tab2", Tab2, true],
+  // ["/tab3", Tab3, true],
+  ["/start", Start, false],
+  ["/signin", SignIn, true],
+  ["/", "/signin", true],
 ];
 export const Routes = () => {
   return (
     <>
-      {routes.map(([path, c, rootRedirect]) => (
-        <>
-          <Route key={path} exact path={path}>
-            {c}
-          </Route>
-          <Route key={path} exact path="/">
-            <Redirect to={path} />
-          </Route>
-        </>
-      ))}
+      {routes.map(([path, Comp_to, exact, rootRedirect]) => {
+        return (
+          <React.Fragment key={path}>
+            <Route exact={exact} path={path}>
+              {typeof Comp_to === "string" ? (
+                <Redirect key={path} to={Comp_to} />
+              ) : (
+                <Comp_to />
+              )}
+            </Route>
+          </React.Fragment>
+        );
+      })}
     </>
   );
 };
