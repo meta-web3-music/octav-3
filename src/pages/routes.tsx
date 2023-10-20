fd3c37ffbc12a8041c063f5a42345f38f1766c9d
Om More <51229945+thisisommore@users.noreply.github.com>
2023-10-20 12:25:04 +0530

    Create artist page (#9)

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,17 +2,38 @@ import { Navigate, createBrowserRouter } from "react-router-dom";
 import Start from "./start";
 import SignIn from "./signin";
 import Home from "./home";
+import Artist from "./artist";
+import Posts from "./posts";
+import Fandom from "./fandom";
 
 export const router = createBrowserRouter([
   {
     path: "/start",
     element: <Start />,
     children: [
-      { path: "home", element: <Home />, index: true },
+      { path: "home", element: <Home /> },
       { path: "", element: <Navigate to="home" /> },
     ],
   },
   { path: "/signin", element: <SignIn /> },
+  {
+    path: "/artist",
+    element: <Artist />,
+    children: [
+      {
+        path: "post",
+        element: <Posts />,
+      },
+      {
+        path: "fandom",
+        element: <Fandom />,
+      },
+      {
+        path: "",
+        element: <Navigate to="post" />,
+      },
+    ],
+  },
   { path: "/", element: <Navigate to="/signin" /> },
   { path: "*", element: <p>Not Found</p> },
 ]);
