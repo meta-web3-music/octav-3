6e1531a7b701d9c683c2072ff245793ebf8442e0
thisisommore <ommore501@gmail.com>
2023-10-21 06:04:00 +0000

    Introduce post component

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,22 @@
+import Post from "./Post";
+
 const Posts = () => {
-  return <div>Posts</div>;
+  return (
+    <div className="p-3">
+      <div className="flex items-center text-[#00000099] ">
+        <div className="border-[1px] border-[#00000099] rounded-md p-1">
+          <span className="iconify" data-icon="iconamoon:search"></span>
+        </div>
+        <span className="text-2xl ml-auto">
+          <span className="iconify" data-icon="bi:filter"></span>
+        </span>
+      </div>
+
+      <div className="my-2"></div>
+      <Post />
+      <Post />
+    </div>
+  );
 };
 
 export default Posts;
